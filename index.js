27c8a4cff5e8d65ccf2df75ce311fa6617e74a08
Vamshi <vamshi3101@gmail.com>
2023-06-09 10:51:03 +0530

    Key press event added

--- index.js
+++ index.js
@@ -17,3 +17,17 @@ for(var i = 0;i<li.length;i++){
         document.querySelector(".inp").value = string;
     })
 }
+document.addEventListener("keydown",(event)=>{
+    if(event.key=='='){
+        try{
+            string = eval(string);
+        }
+        catch(error){
+            string = "Undefined";
+        }
+    }
+    else if(event.key=='C' || event.key=='c')string = "";
+    else if(event.key>='0' && event.key<='9')string+=event.key;
+    else if(event.key=='+' || event.key=='-' || event.key=='/' || event.key=='*')string+=event.key;
+    document.querySelector(".inp").value = string;
+})
